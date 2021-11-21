fe0d37893363e2daf14a2fcb59b8817599035503
murarik57 <murarikumar57@gmail.com>
2021-11-21 16:48:24 +0530

    created Profile model and added feature to get profile data of signed in user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-//@route GET api/profile
-//@descp Test route
-//@access Public
-router.get("/", (req, res) => {
-  res.send("profile Routes");
+//@route GET api/profile/me
+//@descp  GET current users profile
+//@access Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
