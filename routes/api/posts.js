a4ee54d9645a0e68424c3465ed83dea48d0ee147
murarik57 <murarikumar57@gmail.com>
2021-11-22 11:06:33 +0530

    delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const Profile = require("../../models/Profile");
 //@route POST api/posts
 //@descp create a post
 //@access Public
-router.get(
+router.post(
   "/",
   [auth, [check("text", "Post text body is required").notEmpty()]],
   async (req, res) => {
@@ -36,4 +36,57 @@ router.get(
   }
 );
 
+//@route GET api/posts
+//@descp get all post
+//@access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route GET api/posts/:id
+//@descp get all post by id
+//@access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(400).json({ msg: "Post not found" });
+    res.json(post);
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Post not found" });
+    }
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route DELETE api/posts/:id
+//@descp delete a post by its id
+//@access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(400).json({ msg: "Post not found" });
+    }
+    //check user owns the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    await post.remove();
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Post not found" });
+    }
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
